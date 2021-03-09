d3932365da0cc12df2b795727ca914f4d936f3ee
Eduard Gavrila <edygavrila@gmail.com>
2021-03-09 23:11:24 +0000

    Changed the swapping of variables

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,9 +3,7 @@ num_arr = [4,3,78,2,0,2]
 def bubble_sort(arr)
     for i in 0..arr.length-2
         if arr[i] > arr[i+1]
-            aux = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = aux
+           arr[i], arr[i+1] = arr[i+1], arr[i]
             bubble_sort(arr)
         end
     end
